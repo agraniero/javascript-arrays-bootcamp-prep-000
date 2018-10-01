44aef519e126c327e130a30a07c06d0b7a516e4a
Anthony Graniero <ananfro@gmail.com>
2018-10-01 18:23:36 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ['snickers', 'hudred grand', 'kitkat', 'skittles'];
+
+var newElement = 'whatchamacallit';
+
+function addElementToBeginningOfArray(chocolateBars, newElement) {
+  return [newElement, ...chocolateBars];
+}
+
+var cats = ["Milo", "Garfield"];
+var otherNewElement = "Felix";
+
+function destructivelyAddElementToBeginningOfArray(cats, otherNewElement) {
+  cats.unshift(otherNewElement);
+  return cats;
+}
+
